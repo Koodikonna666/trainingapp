c9673b9d2c8b7fef3f487e4be6d4b1fed36b6718
Oskari Lehtonen <lehtonen.osku@gmail.com>
2019-02-24 12:06:02 +0200

    Ensimmäinen git

---
+++ trainingapp/src/main/java/hh/palvelinohjelmointi/trainingapp/TrainingappApplication.java
@@ -0,0 +1,13 @@
+package hh.palvelinohjelmointi.trainingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingappApplication.class, args);
+	}
+
+}
